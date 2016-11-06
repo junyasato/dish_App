c2a1d102923c7bbc67e83460ad882ce87651d99a
Sato Junya <sjtew0071@gmail.com>
2016-11-06 19:14:51 +0900

    aa

--- config/application.rb
+++ config/application.rb
@@ -11,14 +11,5 @@ module MealApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-config.assets.precompile += %w( pc/css/jTinder.css )
-config.assets.precompile += %w( pc/js/jquery.min.js )
-config.assets.precompile += %w( pc/js/jquery.transform2d.js )
-config.assets.precompile += %w( pc/js/jquery.jTinder.js )
-
-config.assets.precompile += %w( sp/css/jTinder.css )
-config.assets.precompile += %w( sp/js/jquery.min.js )
-config.assets.precompile += %w( sp/js/jquery.transform2d.js )
-config.assets.precompile += %w( sp/js/jquery.jTinder.js )
   end
 end
