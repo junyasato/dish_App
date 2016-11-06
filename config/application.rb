30759db1e4e7506f338a37aa3a4ff92b5ac43108
Sato Junya <sjtew0071@gmail.com>
2016-11-06 19:11:48 +0900

    sad

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+module MealApp
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
 config.assets.precompile += %w( pc/css/jTinder.css )
 config.assets.precompile += %w( pc/js/jquery.min.js )
 config.assets.precompile += %w( pc/js/jquery.transform2d.js )
@@ -15,11 +20,5 @@ config.assets.precompile += %w( sp/css/jTinder.css )
 config.assets.precompile += %w( sp/js/jquery.min.js )
 config.assets.precompile += %w( sp/js/jquery.transform2d.js )
 config.assets.precompile += %w( sp/js/jquery.jTinder.js )
-
-module MealApp
-  class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
   end
 end
